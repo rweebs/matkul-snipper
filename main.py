51036fe0376ec4b287aacf95281594d2e869a9f7
widyaput <wnugrah.putra@gmail.com>
2022-01-07 08:23:17 +0700

    feat: add linux command, add poetry file, fix: fixing index for pendaftar, docs: edit readme

--- main.py
+++ main.py
@@ -2,34 +2,42 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import os
-iteration=1
+
+iteration = 1
 from os import environ as env
 from dotenv import load_dotenv
+
 load_dotenv()
+
+
+def alert():
+    host_os = env["OS"]
+    command = "start"
+    if host_os == "LINUX":
+        command = "xdg-open"
+    if host_os == "MAC":
+        command = "open"
+    os.system("{} https://www.youtube.com/watch?v=dQw4w9WgXcQ".format(command))
+
 
 while True:
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time)
-    page = requests.get(env["URL_MK"], headers={"cookie":env["COOKIE"],"user-agent":env["USER_AGENT"]})
+    page = requests.get(
+        env["URL_MK"],
+        headers={"cookie": env["COOKIE"], "user-agent": env["USER_AGENT"]},
+    )
     soup = BeautifulSoup(page.content, "html.parser")
-    kelas=int(env["KELAS"])-1
-    scrapper = soup.find_all("p",  {"class": ["small"]})
-    kuota = scrapper[kelas*2].text
-    pendaftar = scrapper[kelas*2-1].text
+    kelas = int(env["KELAS"]) - 1
+    scrapper = soup.find_all("p", {"class": ["small"]})
+    kuota = scrapper[kelas * 2].text
+    pendaftar = scrapper[kelas * 2 + 1].text
     print(kuota)
     print(pendaftar)
-    host_os=env["OS"]
-    command="start"
-    if (host_os=="MAC"):
-        command="open"
-    if(int(pendaftar.split(" ")[1])<int(kuota.split(" ")[1])):
-        if (host_os=="MAC"):
-            os.system("{} 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'".format(command))
-        else:
-            os.system("{} https://www.youtube.com/watch?v=dQw4w9WgXcQ".format(command))
-    print("Banyak iterasi",iteration)
-    iteration+=1
-    #Lama Jeda dalam detik
+    if int(pendaftar.split(" ")[1]) < int(kuota.split(" ")[1]):
+        alert()
+    print("Banyak iterasi", iteration)
+    iteration += 1
+    # Lama Jeda dalam detik
     time.sleep(60)
-
